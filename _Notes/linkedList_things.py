6890e0b86ddbad72bf3554ab05d3dd61bbc62a78
M.Ber <136386193+cmlbrrss@users.noreply.github.com>
2024-05-03 04:33:16 +0800

    Update linkedList_things.py

--- _Notes/linkedList_things.py
+++ _Notes/linkedList_things.py
@@ -3,30 +3,43 @@ Linked List(鏈結串列)
 
 1. Node資料型態、記憶體大小不必相同
 2. 只能做循序存取 (Sequential access)
-3. 實際上python沒有SingleLinkedList、ListNode要自己宣告
-4. 常用可以宣告有head(第一個node)、tail(最後一個node)、current(當前node)，進行撰寫
+3. 實際上python沒有SingleLinkedList要自己宣告
+4. 常用可以宣告head(第一個node)、tail(最後一個node)、current(當前node)，方便進行撰寫
+5. 每個ListNode的組成為val(這個Node的值)與Next(下一個Node的資訊)，因為Next包含了下一個Node值與他的Next，所以某個Node的next其實等同於儲存了此node以後所有node的資訊
 
 圖示:
-    (1) -> (2) -> (3) -> NULL
-    其中(1)為head，(3)為tail，最後指向NULL
+    (1) -> (2) -> (3) -> None
 
-注意:
+實際結構，以[1 2 3 4 5]為例:
+    ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: ListNode{val: 5, next: None}}}}}
+    所以[1 2 3 4 5].next實際上就是[2 3 4 5]
+
+說明:
 1. 對Node進行增加或修改時，通常是對(某個Node).next下指令
     如:
-        增加一個Node到尾端: self.tail.next = newNode
-        刪除一個Node: self.tail.next = None
-2. 找到鍊表中的特定位置可以使用以下Pattern
-        對self.head的部分進行處理    
-        currentNode = self.head
-        while 特定位置的否定條件(用ctr找特定位置: ctr != N-1，tail位置可以用None: currentNode.next != None):
+        增加一個Node到尾端: (倒數第二個Node).next = newNode
+        刪除一個Node到尾端: (倒數第二個Node).next = None
+2. 最後要return整個鏈表就要return第一個Node(head)
+3. 找到鍊表中的特定位置可以使用以下Pattern
+        對head的部分進行處理(因為沒有head沒有上一個node讓他next)
+        currentNode = head
+        while 特定位置的否定條件:    (例如: 用ctr找特定位置 -> ctr != N-1，最後的位置可以用None -> currentNode.next != None)
             currentNode = currentNode.next
-        (對currentNode.next進行目標操作)  
-3. 若沒有宣告class SingleLinkedList(包含head、tail等)，一個linkedList的head
+        (進行目標操作)  
+        return head
+4. 如果要slice，例:[1 2 3 4 5]保留[1 2 3 5]
+        先用第二點中，while配合currentNode = currentNode.next到達currentNode指向Node3
+        利用宣告變數的方式暫存某段的鏈表
+        first3Nodes = currentNode
+        forthNode = currentNode.next 
+        first3Nodes.next = forthNode.next
+        最後return head
+5. 若沒有宣告class SingleLinkedList(包含head、tail等)，一個已宣告的linkedList可直接對其操作，因為會直接指向第一個node
 """
 
 class Node:
     def __init__(self ,data=None, next=None):
-      self.data = data
+      self.val = data
       self.next = next
 
 class SingleLinkedList:
