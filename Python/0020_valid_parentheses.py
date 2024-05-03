8f1d9c9ece3768dcdc254502523d0428f3cbee94
M.Ber <136386193+cmlbrrss@users.noreply.github.com>
2024-05-03 23:06:44 +0800

    Update 0020_valid_parentheses.py

--- Python/0020_valid_parentheses.py
+++ Python/0020_valid_parentheses.py
@@ -1,5 +1,5 @@
 """
-My ans
+My ans(Slow)
 """
 class Solution(object):
     def isValid(self, s):
@@ -23,4 +23,32 @@ class Solution(object):
             else:
                 return False
         return True
+
+"""
+My ans(Fast)
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mapping = {'(':')', '[':']', '{':'}'}
+        if len(s) % 2 != 0:
+            return False
+        i = 0
+        close = ''
+        for i in range(len(s)):
+            if s[i] in mapping.keys():
+                close += mapping[s[i]]
+            else:
+                if close == '' or s[i] != close[-1]:
+                    return False
+                else:
+                    close = close[:-1]
+        if close == '':
+            return True
+        else:
+            return False
 
