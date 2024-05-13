ce594130922d1c7849fda72dcf24541ce5ca9d63
M.Ber <136386193+cmlbrrss@users.noreply.github.com>
2024-05-13 17:07:26 +0800

    Update 0018_4Sum.py

--- Python/0018_4Sum.py
+++ Python/0018_4Sum.py
@@ -1,5 +1,12 @@
 """
+題目: 給定一個數列與目標，找出所有4數總和為目標的組合，不可重複
+"""
+
+"""
 My ans (For迴圈+雙指針)
+重點:
+1. 雙指針為一個把時間複雜度從n^x降為n^(x - 1)的演算法
+2. 先用兩個for迴圈固定a + b，再去找c、d，同三數之和
 """
 class Solution(object):
     def __init__(self):
@@ -9,15 +16,15 @@ class Solution(object):
         for j in range(a_idx+1,len(nums)):
             b = nums[j]
             print(b,nums[j-1])
-            if j >= a_idx+2 and b == nums[j-1]:
+            if j >= a_idx+2 and b == nums[j-1]:   #同 if i >= 1 and a == nums[i-1]，去重
                 continue
-            if a + b > target and target > 0:
+            if a + b > target and target > 0:     #同 if a > target and target > 0，因為a + b已大於目標所以省去驗證其他組合
                 break
             l = j + 1
             r = len(nums) - 1
             while l < r:
-                c = nums[l]
-                d = nums[r]
+                c = nums[l]     # c為左指針(較小)
+                d = nums[r]     # d為右指針(較大)
                 if a + b + c + d == target:
                     self.ans.append([a, b, c, d])
                     while l < r and c == nums[l + 1]: #跳過相同的c，因為a、b固定，c、d數對也固定
