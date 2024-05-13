e46df5398989482e969809cf5d3c97d80d0ded85
M.Ber <136386193+cmlbrrss@users.noreply.github.com>
2024-05-13 18:09:11 +0800

    Update 0034_searchRange.py

--- Python/0034_searchRange.py
+++ Python/0034_searchRange.py
@@ -1,5 +1,30 @@
 """
+題目:給定一個遞增數列(非嚴格遞增)與目標，返回目標的最小與最大Index
+"""
+
+"""
 My Ans
+重點:
+使用遞迴函數時，要return接自我調用，否則會return None
+
+錯誤寫法，如:
+    def func(...):
+        if cond:
+            self.func(...) 
+        else:
+            return ...    
+舉例可參考: https://blog.csdn.net/Ha_hha/article/details/79393041
+理由:
+最後一個遞迴(假設為第N個遞迴)有正常return，然後程式就會回去跑第N - 1個遞迴剩下的指令，但剩餘指令中未執行到其他return，所以第N - 1個遞迴return值為None，同理第N - 2, 第N - 3...第1個遞迴
+第1個遞迴因為return值為None，所以最後輸出為None而非正常return。
+
+整確的寫法，要return接自我調用，如:
+    def func(...):
+        if cond:
+            return self.func(...)
+        else:
+            return ...
+        
 """
 class Solution(object):
     def __init__(self):
